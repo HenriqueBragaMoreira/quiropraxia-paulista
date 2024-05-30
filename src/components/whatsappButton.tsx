0aa7ba58a647b11587379bfc02514df9b91de5d0
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-05-30 20:48:43 -0300

    refactor: 💡 redone the whatsappButton component

--- src/components/whatsappButton.tsx
+++ src/components/whatsappButton.tsx
@@ -1,44 +1,31 @@
 import { Whatsapp } from './icon/brands';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from './ui/dropdown-menu';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 export function WhatsappButton() {
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+    <Popover>
+      <PopoverTrigger asChild>
         <button
-          className="fixed bottom-8 right-12 bg-white rounded-full p-4 hover:bg-white/60 ease-in duration-200"
+          className="fixed bottom-8 right-16 bg-white rounded-full p-4 hover:bg-white/60 ease-in duration-200"
           title="Link para o whatsapp da clínica"
         >
           <Whatsapp />
         </button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Nossos contatos</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem asChild>
-          <a
-            href="https://wa.me/5511911050123?text=Olá,%20vim%20pelo%20site%20e%20quero%20marcar%20uma%20consulta!"
-            target="_blank"
-            className="gap-2"
-          >
-            <Whatsapp width={16} height={16} />
-            Clinica Paulista
-          </a>
-        </DropdownMenuItem>
-        <DropdownMenuItem asChild>
-          <a className="gap-2" href="">
-            <Whatsapp width={16} height={16} />
-            Clinica Pinheiros
-          </a>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <a
+          href="https://wa.me/5511911050123?text=Olá,%20vim%20pelo%20site%20e%20quero%20marcar%20uma%20consulta!"
+          target="_blank"
+          className="flex items-center gap-2 hover:bg-black/5 p-2"
+        >
+          <Whatsapp width={16} height={16} />
+          Clinica Paulista
+        </a>
+        <a className="flex items-center gap-2 hover:bg-black/5 p-2" href="">
+          <Whatsapp width={16} height={16} />
+          Clinica Pinheiros
+        </a>
+      </PopoverContent>
+    </Popover>
   );
 }
