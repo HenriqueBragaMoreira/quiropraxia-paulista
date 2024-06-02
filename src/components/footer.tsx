dce4aaebe0c47ea568da97e52fa44427c89afabd
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-06-02 15:28:52 -0300

    refactor: 💡 add responsiveness in footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ import { Logo } from './icon/logo';
 export function Footer() {
   return (
     <footer className="flex flex-col bg-[#1F1F1F] py-4 px-8">
-      <div className="flex justify-between border-b pb-5">
-        <div className="max-w-[32rem]">
+      <div className="flex flex-col lg:flex-row justify-between border-b pb-5 items-center">
+        <div className="max-w-[30rem] lg:max-w-[32rem]">
           <h1 className="text-xl font-bold">Benefícios da Quiropraxia</h1>
           <span className="text-gray-300">
             A quiropraxia alivia dores na coluna e melhora a postura através de ajustes manuais,
@@ -28,7 +28,7 @@ export function Footer() {
           </span>
         </div>
       </div>
-      <div className="flex items-center justify-between pt-5">
+      <div className="flex flex-col gap-10 lg:flex-row items-center justify-between pt-5">
         <Logo className="w-[200px]" />
         <div className="flex flex-col items-center">
           <span className="text-gray-200 text-xs">
@@ -36,7 +36,7 @@ export function Footer() {
           </span>
           <span className="text-gray-200 text-xs">Todos os direitos reservados.</span>
         </div>
-        <div className="flex gap-8">
+        <div className="flex flex-col xs:flex-row items-center gap-8">
           {routes.map((item) => (
             <Link
               className="font-medium hover:text-[#00667F] hover:underline duration-300"
