f784d3bb87f28e9efeda8281a36f89a8b2368034
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-06-03 12:22:26 -0300

    feat: 🎸 add animation in second content

--- src/app/components/secondContent.tsx
+++ src/app/components/secondContent.tsx
@@ -1,31 +1,38 @@
+import { WhileImageInViewAnimation, WhileInViewAnimation } from '@/components/animation';
+
 export function SecondContent() {
   return (
     <div
       id="second-content"
-      className="2xl:h-screen mt-20 pt-20 md:mt-0 md:pt-[88px] bg-[#00667F]/20">
+      className="3xl:h-screen mt-20 pt-20 md:mt-0 md:pt-[88px] bg-[#00667F]/20"
+    >
       <div className="flex flex-col items-center gap-2 md:gap-8 px-3 md:px-10 py-6">
-        <h2 className="text-3xl md:text-5xl font-bold">Quem Somos</h2>
-        <div className="flex flex-col gap-4 text-sm md:text-lg text-center">
-          <span>
-            Na Quiropraxia Paulista focamos em tratar disfunções da coluna e articulações, nosso
-            objetivo é otimizar o funcionamento do corpo para melhor qualidade de vida.
-          </span>
-          <span>
-            Somos uma das maiores redes de clínicas de Quiropraxia em SP, oferecendo tratamento
-            acessível e centrado no paciente, nossa abordagem única visa alívio da dor e cuidados
-            preventivos.
-          </span>
-          <span>
-            Como uma empresa familiar, nos dedicamos à saúde e qualidade de vida dos pacientes,
-            proporcionando excelência e cuidado personalizado.
-          </span>
-          <span>Venha descobrir uma nova forma de viver sem dor conosco. 😉</span>
-        </div>
-        <div className="flex flex-col gap-10 xs:grid xs:grid-cols-2 md:flex md:gap-0 md:flex-row md:justify-between items-center md:w-full">
+        <WhileInViewAnimation>
+          <h2 className="text-3xl md:text-5xl font-bold">Quem Somos</h2>
+        </WhileInViewAnimation>
+        <WhileInViewAnimation>
+          <div className="flex flex-col gap-4 text-sm md:text-lg text-center">
+            <span>
+              Na Quiropraxia Paulista focamos em tratar disfunções da coluna e articulações, nosso
+              objetivo é otimizar o funcionamento do corpo para melhor qualidade de vida.
+            </span>
+            <span>
+              Somos uma das maiores redes de clínicas de Quiropraxia em SP, oferecendo tratamento
+              acessível e centrado no paciente, nossa abordagem única visa alívio da dor e cuidados
+              preventivos.
+            </span>
+            <span>
+              Como uma empresa familiar, nos dedicamos à saúde e qualidade de vida dos pacientes,
+              proporcionando excelência e cuidado personalizado.
+            </span>
+            <span>Venha descobrir uma nova forma de viver sem dor conosco. 😉</span>
+          </div>
+        </WhileInViewAnimation>
+        <WhileImageInViewAnimation>
           <div className="h-64 w-56 md:h-72 md:w-[232px] lg:h-96 lg:w-80 bg-gray-500 rounded-lg shadow-2xl" />
           <div className="h-64 w-56 md:h-72 md:w-[232px] lg:h-96 lg:w-80 bg-gray-500 rounded-lg shadow-2xl" />
           <div className="h-64 w-56 md:h-72 md:w-[232px] lg:h-96 lg:w-80 bg-gray-500 rounded-lg shadow-2xl" />
-        </div>
+        </WhileImageInViewAnimation>
       </div>
     </div>
   );
--- src/components/animation.tsx
+++ src/components/animation.tsx
@@ -53,3 +53,42 @@ export function NetworkAnimation({ children }: WithChildren) {
     </motion.div>
   );
 }
+
+export function WhileInViewAnimation({ children }: WithChildren) {
+  return (
+    <motion.div
+      variants={{
+        hidden: { opacity: 0, x: -75 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{
+        once: true,
+      }}
+      transition={{ duration: 0.75, delay: 0.25 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export function WhileImageInViewAnimation({ children }: WithChildren) {
+  return (
+    <motion.div
+      className="flex flex-col gap-10 xs:grid xs:grid-cols-2 md:flex md:gap-0 md:flex-row md:justify-between items-center md:w-full"
+      variants={{
+        hidden: { opacity: 0, x: -75 },
+        visible: { opacity: 1, x: 0 },
+      }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{
+        once: true,
+      }}
+      transition={{ duration: 0.75, delay: 0.25 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
