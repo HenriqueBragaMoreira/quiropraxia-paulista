bbf81eb85dac75965d0c5aaf4abcc1afa5c7b6bd
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-06-05 13:06:57 -0300

    feat: 🎸 add animation in third content

--- src/app/components/thirdContent/index.tsx
+++ src/app/components/thirdContent/index.tsx
@@ -1,21 +1,26 @@
 import { InfiniteMovingCards } from '@/components/ui/infinite-moving-cards';
 import { testimonials } from '@/utils/testimonials';
+import { WhileBlockInViewAnimation, WhileInViewAnimation } from './animation';
 
 export function ThirdContent() {
   return (
     <div id="third-content" className="3xl:h-screen pt-20 md:pt-[88px]">
       <div className="flex flex-col gap-10 px-3 md:px-10">
         <div className="space-y-6">
-          <h2 className="text-3xl md:text-5xl font-bold text-center">Nossos Tratamentos</h2>
-          <div className="flex flex-col gap-10 xs:grid xs:grid-cols-2 md:flex md:gap-0 md:flex-row md:justify-between items-center md:w-full xs:justify-items-center">
+          <WhileInViewAnimation>
+            <h2 className="text-3xl md:text-5xl font-bold text-center">Nossos Tratamentos</h2>
+          </WhileInViewAnimation>
+          <WhileBlockInViewAnimation>
             <div className="h-64 w-52 lg:h-72 lg:w-64 bg-gray-500 rounded-lg shadow-2xl" />
             <div className="h-64 w-52 lg:h-72 lg:w-64 bg-gray-500 rounded-lg shadow-2xl" />
             <div className="h-64 w-52 lg:h-72 lg:w-64 bg-gray-500 rounded-lg shadow-2xl" />
             <div className="h-64 w-52 lg:h-72 lg:w-64 bg-gray-500 rounded-lg shadow-2xl" />
-          </div>
+          </WhileBlockInViewAnimation>
         </div>
         <div className="flex flex-col w-full gap-6 items-center">
-          <h2 className="text-3xl md:text-5xl font-bold text-center">Feedback dos Pacientes</h2>
+          <WhileInViewAnimation>
+            <h2 className="text-3xl md:text-5xl font-bold text-center">Feedback dos Pacientes</h2>
+          </WhileInViewAnimation>
           <InfiniteMovingCards
             className="w-full"
             items={testimonials}
