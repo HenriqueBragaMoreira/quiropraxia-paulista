a8a81b7006f4b947f55262fb2b3915d952dfc7dc
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-07-16 22:37:51 -0300

    feat: 🎸 add Google Tag Manager scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,28 @@ export default function RootLayout({
 }>) {
   return (
     <html className="scroll-smooth" lang="pt-BR" suppressHydrationWarning>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N85CZ5H');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N85CZ5H"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         {children}
         <SpeedInsights />
       </body>
