4d3c190dc98352d2b777fe698a2a01a73a88c269
HenriqueBragaMoreira <shenrique40moreira@gmail.com>
2024-10-03 17:28:25 -0300

    refactor: 💡 removed the card from the 5 session plan

--- src/app/components/thirdContent/index.tsx
+++ src/app/components/thirdContent/index.tsx
@@ -23,58 +23,6 @@ export function ThirdContent() {
                 <div className="flex flex-col justify-between h-64 w-52 lg:h-72 lg:w-64 bg-[#154359] rounded-lg shadow-2xl py-6 px-5 border-[4px] border-white">
                   <div className="flex flex-col items-center gap-4">
                     <div>
-                      <h2 className="text-2xl font-bold text-center">5</h2>
-                      <span className="font-semibold">Sessões</span>
-                    </div>
-                    <span className="text-xs lg:text-sm">
-                      Comece com nosso pacote introdutório de Quiropraxia e experimente os
-                      benefícios de ajustes regulares.
-                    </span>
-                  </div>
-                  <Button className="bg-white text-black hover:bg-white/80">
-                    Mais informações
-                  </Button>
-                </div>
-              }
-              backContent={
-                <div className="flex flex-col gap-1 lg:gap-3 h-full w-full bg-[#154359] rounded-lg shadow-2xl py-6 px-5 border-[4px] border-white">
-                  <div className="flex justify-between w-full">
-                    <ChevronLeft name="Icone para voltar" className="cursor-pointer" />
-                    <span>Preços</span>
-                    <div />
-                  </div>
-                  <div className="flex flex-col gap-1 lg:gap-2">
-                    <div className="flex gap-4 justify-center items-center">
-                      <Check size={18} />
-                      <span className="text-xs">Tratamento Personlizado!</span>
-                    </div>
-                    <div className="flex gap-4 justify-center items-center">
-                      <Check size={18} />
-                      <span className="text-xs">5 sessões de quiropraxia!</span>
-                    </div>
-                  </div>
-                  <div className="flex flex-col justify-center items-center lg:gap-2 mt-1 lg:mt-3">
-                    <span className="lg:text-xl font-semibold">R$760,00 á vista</span>
-                    <span>ou</span>
-                    <span>4x de R$200,00</span>
-                  </div>
-                  <a
-                    href="https://api.whatsapp.com/send?phone=5511911050123&text=Ol%C3%A1,%20vim%20pelo%20site%20e%20quero%20adquirir%20aqui%20com%20voc%C3%AAs%C2%A0o%C2%A0plano%C2%A05!"
-                    target="_blank"
-                    className="flex justify-around text-xs lg:text-sm font-semibold text-center mt-1 lg:mt-0 py-1 lg:py-2 bg-white hover:bg-white/80 text-black rounded-lg items-center cursor-pointer"
-                  >
-                    <Whatsapp className="size-5" />
-                    Adquira agora!
-                  </a>
-                </div>
-              }
-            />
-
-            <FlippingCard
-              frontContent={
-                <div className="flex flex-col justify-between h-64 w-52 lg:h-72 lg:w-64 bg-[#154359] rounded-lg shadow-2xl py-6 px-5 border-[4px] border-white">
-                  <div className="flex flex-col items-center gap-4">
-                    <div>
                       <h2 className="text-2xl font-bold text-center">10</h2>
                       <span className="font-semibold">Sessões</span>
                     </div>
